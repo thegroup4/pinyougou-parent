283065962cfa3cb1251dc1eb2b92f49c9920e60a
123 <123@163.com>
2019-04-11 23:41:19 +0800

    zlm_test

--- pinyougou-web-user/src/main/java/cn/itcast/core/controller/LoginController.java
+++ pinyougou-web-user/src/main/java/cn/itcast/core/controller/LoginController.java
@@ -1,12 +1,9 @@
 package cn.itcast.core.controller;
 
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +22,7 @@ public class LoginController {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         System.out.println(111111);
         System.out.println(111111);
+        System.out.println(1111111112);
         Map<String,Object> map = new HashMap<>();
         map.put("loginName",name);
        // map.put("curTime",new Date());
